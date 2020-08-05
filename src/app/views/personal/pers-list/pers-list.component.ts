43c2b84a5990987a292eb6aea00d032282030a9d
TheDenky <denkyruben@gmail.com>
2020-08-04 23:38:54 -0400

    datatables funcionando

--- src/app/views/personal/pers-list/pers-list.component.ts
+++ src/app/views/personal/pers-list/pers-list.component.ts
@@ -3,42 +3,51 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 
-export interface PeriodicElement {
-  DNI: string;
-  Nombres: string;
-  apepat: string;
-  apemat: string;
-  Jornada: string;
-  Contrato: string;
-  Cargo: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [];
+import { Personal } from '../../../models/personal';
+import { PersonalService } from '../../../services/personal.service';
 
 @Component({
   selector: 'app-pers-list',
   templateUrl: './pers-list.component.html',
   styleUrls: ['./pers-list.component.scss'],
+  providers: [PersonalService],
 })
 export class PersListComponent implements OnInit {
+  ELEMENT_DATA: Personal[];
   displayedColumns: string[] = [
-    'DNI',
-    'Nombres',
-    'apepat',
-    'apemat',
-    'Cargo',
-    'Contrato',
-    'Jornada',
+    'dniPers',
+    'nombPers',
+    'apelPatePers',
+    'apelMatePers',
+    'cargPers',
+    'contLaboPers',
+    'jornLaboPers',
     'ACTION',
   ];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<Personal>(this.ELEMENT_DATA);
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {}
+  constructor(private personalService: PersonalService) {}
+  unapersona: any = [];
 
   ngOnInit(): void {
+    this.obtenerPersonal();
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+  public obtenerPersonal() {
+    let resp = this.personalService.getPersonal();
+    resp.subscribe((report) => (this.dataSource.data = report as Personal[]));
+  }
+  public obtenerUnPersonal(id: string) {
+    this.personalService.getOnePersonal(id).subscribe(
+      (res) => {
+        console.log(res);
+        this.unapersona = res;
+      },
+      (err) => console.error(err)
+    );
   }
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
